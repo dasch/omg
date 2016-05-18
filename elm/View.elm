e8f192e05f57142713dc82ddda338354eca21f73
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2016-05-18 14:25:21 -0700

    Send message on enter

--- elm/View.elm
+++ elm/View.elm
@@ -4,6 +4,7 @@ module View exposing (view)
 import Html exposing (Html, div, text, button)
 import Html.Attributes exposing (placeholder, value)
 import Html.Events exposing (onInput, onClick)
+import Json.Decode
 
 import Model exposing (Model)
 import Update exposing (Msg(Input, Send))
@@ -24,6 +25,7 @@ view model =
             [ Html.input
                 [ placeholder "Text"
                 , onInput Input
+                , onEnter Send
                 , value model.entry
                 ]
                 []
@@ -34,3 +36,20 @@ view model =
         [ div [] messages
         , div [] [input]
         ]
+
+
+onKeyDown : Int -> msg -> Html.Attribute msg
+onKeyDown keycode msg =
+  let
+      decoder =
+        Html.Events.keyCode `Json.Decode.andThen` checkKeyCode
+
+      checkKeyCode kc =
+        if kc == keycode then Json.Decode.succeed msg else Json.Decode.fail "No the right keycode"
+  in
+      Html.Events.on "keydown" decoder
+
+
+onEnter : msg -> Html.Attribute msg
+onEnter msg =
+  onKeyDown 13 msg
